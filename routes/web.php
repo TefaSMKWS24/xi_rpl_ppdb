1737a123942cee33073ffc687ff45ada17f383b4
TefaSMKWS24 <tefaproject.smKws@gmail.com>
2025-02-01 14:11:11 -0800

    modified:   routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BiodataOrtuController;
 use App\Http\Controllers\BiodataSiswaController;
 use App\Http\Controllers\DataNilaiController;
-use App\Http\Controllers\DataortuController;
+use App\Http\Controllers\DataOrtuController;
 use App\Http\Controllers\DataPendidikanController;
 use App\Http\Controllers\DataPrestasiController;
 use App\Http\Controllers\DataSiswaController;
@@ -25,6 +25,18 @@ use App\Http\Controllers\TabelBerkarController;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/guru/index', function () {
+    return view('guru.index');
 });
+
+route::resource('biodata_ortu',BiodataOrtuController::class);
+route::resource('biodata_siswa',BiodataSiswaController::class);
+route::resource('data_nilai',DataNilaiController::class);
+route::resource('data_ortu',DataOrtuController::class);
+route::resource('data_pendidikan',DataPendidikanController::class);
+route::resource('data_prestasi',DataPrestasiController::class);
+route::resource('data_siswa',DataSiswaController::class);
+route::resource('data_wali',DataWaliController::class);
+route::resource('ekstrakurikuler',EkstrakurikulerController::class);
+route::resource('kesehatan',KesehatanController::class);
+route::resource('tebel_berkas',TabelBerkasController::class);
